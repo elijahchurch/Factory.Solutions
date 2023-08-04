e140f2fd69b554d7e6f98059cc31038d99820139
Jason E. Church <elijahchurch@gmail.com>
2023-08-04 10:21:48 -0700

    Create Splash Page with Home controller and view.

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Factory.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly FactoryContext _db;
+
+        public HomeController(FactoryContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            List<Engineer> engineers = _db.Engineers.ToList();
+            List<Machine> machines = _db.Machines.ToList();
+            ViewBag.Engineers = engineers;
+            ViewBag.Machines = machines;
+            ViewBag.Title = "Dr. Sillystringz's Factory Database";
+            return View();
+        }
+    }
+}
