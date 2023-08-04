552218f95daa4c8a969d6b415c7e7ad2fe36da0c
Jason E. Church <elijahchurch@gmail.com>
2023-08-04 10:55:27 -0700

    Add validation Required attribute to Name and Description properties

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models
 {
     public class Machine
     {
         public int MachineId {get; set;}
+        [Required(ErrorMessage = "A name must be entered!")]
         public string Name { get; set;}
+        [Required(ErrorMessage = "A description must be entered!")]
         public string Description { get; set;}
         public List<EngineerMachine> JoinEntities {get;}
     }
